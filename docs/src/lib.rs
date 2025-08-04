ecc9f30a52d9f82749f120b31136160f4cee91fa
Marti <marti@miden.team>
2025-08-04 13:21:45 +0000

    feat: make counter contract runnable

--- docs/src/lib.rs
+++ docs/src/lib.rs
@@ -1,2 +1,3 @@
 #![doc = include_str!("rust-client/create_deploy_tutorial.md")]
 #![doc = include_str!("rust-client/mint_consume_create_tutorial.md")]
+#![doc = include_str!("rust-client/counter_contract_tutorial.md")]
