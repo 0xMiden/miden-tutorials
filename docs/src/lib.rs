ff55a28ebcff75f1c3cce90fc7a36868c408da08
Marti <marti@miden.team>
2025-08-05 21:46:45 +0000

    chore: make fpi runnable

--- docs/src/lib.rs
+++ docs/src/lib.rs
@@ -3,3 +3,4 @@
 #![doc = include_str!("rust-client/counter_contract_tutorial.md")]
 #![doc = include_str!("rust-client/public_account_interaction_tutorial.md")]
 #![doc = include_str!("rust-client/custom_note_how_to.md")]
+#![doc = include_str!("rust-client/foreign_procedure_invocation_tutorial.md")]
