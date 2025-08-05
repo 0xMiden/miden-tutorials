66b6cdfae894e0968ddd93c07a68ff441d27ba79
Marti <marti@miden.team>
2025-08-05 15:19:09 +0000

    fix: make pub interaction runnable

--- docs/src/lib.rs
+++ docs/src/lib.rs
@@ -1,3 +1,4 @@
 #![doc = include_str!("rust-client/create_deploy_tutorial.md")]
 #![doc = include_str!("rust-client/mint_consume_create_tutorial.md")]
 #![doc = include_str!("rust-client/counter_contract_tutorial.md")]
+#![doc = include_str!("rust-client/public_account_interaction_tutorial.md")]
