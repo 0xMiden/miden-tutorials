a65561a8dbea78e45f628c29c22add3f77e80f49
keinberger <philipp.keinberger@gmail.com>
2025-09-11 09:23:02 +0300

    chore: third error POC

--- web-client/lib/constants.ts
+++ web-client/lib/constants.ts
@@ -1,2 +1,2 @@
-export const NODE_URL = "http://localhost:57291";
+export const NODE_URL = "https://rpc.testnet.miden.io:443";
 export const TIC_TAC_TOE_CONTRACT_ID = "mtst1qz43ftxkrzcjsqz3hpw332qwny2ggsp0";
