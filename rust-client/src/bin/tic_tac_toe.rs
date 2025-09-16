2e6c0e900ccd4daa997b311ddf2dcdab6e2c1c71
keinberger <philipp.keinberger@gmail.com>
2025-09-16 12:00:16 +0300

    feat: new tic tac toe contract with working make_a_move and constructor procedures

--- rust-client/src/bin/tic_tac_toe.rs
+++ rust-client/src/bin/tic_tac_toe.rs
@@ -130,15 +130,17 @@ async fn main() -> Result<(), ClientError> {
         game_code.clone(),
         assembler,
         vec![
-            // player1 storage slot
-            empty_storage_slot.clone(),
-            // player2 storage slot
-            empty_storage_slot.clone(),
-            // flag storage slot
-            empty_storage_slot.clone(),
-            // winner storage slot
-            empty_storage_slot.clone(),
-            // mapping storage slot
+            // nonce storage slot
+            empty_storage_slot,
+            // player ids mapping storage slot
+            storage_slot_map.clone(),
+            // player1 values mapping storage slot
+            storage_slot_map.clone(),
+            // player2 values mapping storage slot
+            storage_slot_map.clone(),
+            // winners mapping storage slot
+            storage_slot_map.clone(),
+            // winning lines mapping storage slot
             storage_slot_map,
         ],
     )
@@ -173,17 +175,9 @@ async fn main() -> Result<(), ClientError> {
         .unwrap();
 
     // -------------------------------------------------------------------------
-    // STEP 3: Call the Game Contract with a script
+    // STEP 3: Call the Game Contract with the constructor
     // -------------------------------------------------------------------------
     println!("\n[STEP 3] Call Game Contract Constructor");
-
-    // Compose TX script input arguments
-    let deployment_script_arg: [Felt; 4] = [
-        bob_account.id().suffix(),
-        bob_account.id().prefix().as_felt(),
-        alice_account.id().suffix(),
-        alice_account.id().prefix().as_felt(),
-    ];
 
     // Load the MASM script referencing the game deployment procedure
     let deployment_script_path = Path::new("../masm/scripts/game_deployment_script.masm");
@@ -204,15 +198,14 @@ async fn main() -> Result<(), ClientError> {
         .unwrap();
 
     // Build a transaction request with the custom script
-    let tx_game_deployment_request = TransactionRequestBuilder::new()
+    let tx_game_constructor_request = TransactionRequestBuilder::new()
         .custom_script(deployment_script)
-        .script_arg(Word::new(deployment_script_arg.into()))
         .build()
         .unwrap();
 
     // Execute the transaction locally
     let tx_result = client
-        .new_transaction(game_contract.id(), tx_game_deployment_request)
+        .new_transaction(game_contract.id(), tx_game_constructor_request)
         .await
         .unwrap();
 
@@ -225,32 +218,47 @@ async fn main() -> Result<(), ClientError> {
     let account = client.get_account(game_contract.id()).await.unwrap();
     let account_data = account.unwrap().account().clone();
     println!(
-        "game contract player1 storage: {:?}",
+        "nonce storage slot: {:?}",
         account_data.storage().get_item(0)
     );
     println!(
-        "game contract player2 storage: {:?}",
-        account_data.storage().get_item(1)
+        "player ids mapping storage slot: {:?}",
+        account_data.storage().get_map_item(
+            1,
+            Word::new([Felt::new(0), Felt::new(0), Felt::new(0), Felt::new(1)].into())
+        )
     );
     println!(
-        "game contract flag storage: {:?}",
+        "player1 values mapping storage slot: {:?}",
         account_data.storage().get_item(2)
     );
     println!(
-        "game contract winner storage: {:?}",
+        "player2 values mapping storage slot: {:?}",
         account_data.storage().get_item(3)
     );
     println!(
-        "game contract mapping storage: {:?}",
+        "winners mapping storage slot: {:?}",
         account_data.storage().get_item(4)
+    );
+    println!(
+        "winner lines mapping storage slot: {:?}",
+        account_data.storage().get_item(5)
     );
 
     // -------------------------------------------------------------------------
-    // STEP 4: Create "make a move" note
+    // STEP 4: Call the Game Contract with a create game note
     // -------------------------------------------------------------------------
-    println!("\n[STEP 4] Create 'make a move' note");
+    println!("\n[STEP 4] Compose create game note");
 
-    let note_code = fs::read_to_string(Path::new("../masm/notes/make_a_move_note.masm")).unwrap();
+    let assembler: Assembler = TransactionKernel::assembler().with_debug_mode(true);
+    let account_component_lib = create_library(
+        assembler.clone(),
+        "external_contract::game_contract",
+        &game_code,
+    )
+    .unwrap();
+
+    let note_code = fs::read_to_string(Path::new("../masm/notes/create_game_note.masm")).unwrap();
     let note_script = ScriptBuilder::new(true)
         .with_dynamically_linked_library(&account_component_lib)
         .unwrap()
@@ -259,8 +267,11 @@ async fn main() -> Result<(), ClientError> {
 
     let empty_assets = NoteAssets::new(vec![])?;
 
-    let index: u64 = 5;
-    let note_inputs = NoteInputs::new(vec![Felt::new(index)]).unwrap();
+    let note_inputs = NoteInputs::new(vec![
+        bob_account.id().suffix(),
+        bob_account.id().prefix().as_felt(),
+    ])
+    .unwrap();
     let serial_num = client.rng().draw_word();
     let recipient = NoteRecipient::new(serial_num, note_script, note_inputs);
     let tag = NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Local).unwrap();
@@ -271,54 +282,18 @@ async fn main() -> Result<(), ClientError> {
         NoteExecutionHint::always(),
         Felt::new(0),
     )?;
-    let make_a_move_note = Note::new(empty_assets.clone(), metadata, recipient);
+    let create_game_note = Note::new(empty_assets.clone(), metadata, recipient);
 
-    println!("Make a move note ID: {:?}", make_a_move_note.id().to_hex());
-
-    // -------------------------------------------------------------------------
-    // STEP 5: Create "end game" note
-    // -------------------------------------------------------------------------
-    println!("\n[STEP 5] Create 'end game' note");
-
-    let end_game_note_code =
-        fs::read_to_string(Path::new("../masm/notes/end_game_note.masm")).unwrap();
-    let end_game_note_script = ScriptBuilder::new(true)
-        .with_dynamically_linked_library(&account_component_lib)
-        .unwrap()
-        .compile_note_script(end_game_note_code)
-        .unwrap();
-
-    let player_slot: u64 = 1;
-    let end_game_note_inputs = NoteInputs::new(vec![Felt::new(player_slot)]).unwrap();
-    let end_game_serial_num = client.rng().draw_word();
-    let end_game_recipient = NoteRecipient::new(
-        end_game_serial_num,
-        end_game_note_script,
-        end_game_note_inputs,
-    );
-    let end_game_tag = NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Local).unwrap();
-    let end_game_metadata = NoteMetadata::new(
-        alice_account.id(),
-        NoteType::Public,
-        end_game_tag,
-        NoteExecutionHint::always(),
-        Felt::new(0),
-    )?;
-    let end_game_note = Note::new(empty_assets, end_game_metadata, end_game_recipient);
-
-    println!("End game note ID: {:?}", end_game_note.id().to_hex());
+    println!("Create game note ID: {:?}", create_game_note.id().to_hex());
 
     // -------------------------------------------------------------------------
-    // STEP 6: Submit notes on-chain
+    // STEP 5: Submit create game note on-chain
     // -------------------------------------------------------------------------
-    println!("\n[STEP 6] Submit notes on-chain");
+    println!("\n[STEP 5] Submit create game note on-chain");
 
     // build and submit transaction
     let note_request = TransactionRequestBuilder::new()
-        .own_output_notes(vec![
-            OutputNote::Full(make_a_move_note.clone()),
-            OutputNote::Full(end_game_note.clone()),
-        ])
+        .own_output_notes(vec![OutputNote::Full(create_game_note.clone())])
         .build()
         .unwrap();
     let tx_result = client
@@ -328,17 +303,16 @@ async fn main() -> Result<(), ClientError> {
     let _ = client.submit_transaction(tx_result.clone()).await;
     client.sync_state().await?;
 
-    println!("Submitted make a move note AND end game note");
+    println!("Submitted create game note");
 
     // -------------------------------------------------------------------------
-    // STEP 7: Call the Game Contract with notes
+    // STEP 6: Call Game Contract with create game note
     // -------------------------------------------------------------------------
-    println!("\n[STEP 7] Call Game Contract with all notes");
+    println!("\n[STEP 6] Call Game Contract with create game note");
 
-    println!("Consuming note as beneficiary");
+    println!("Consuming create game note as beneficiary");
     let consume_custom_request = TransactionRequestBuilder::new()
-        .unauthenticated_input_notes([(make_a_move_note, None)])
-        // .unauthenticated_input_notes([(make_a_move_note, None), (end_game_note, None)])
+        .unauthenticated_input_notes([(create_game_note, None)])
         .build()
         .unwrap();
     let tx_result = client
@@ -348,10 +322,106 @@ async fn main() -> Result<(), ClientError> {
     let _ = client.submit_transaction(tx_result.clone()).await;
     client.sync_state().await?;
 
-    let account = client.get_account(game_contract.id()).await.unwrap();
+    // -------------------------------------------------------------------------
+    // STEP 7: Call the Game Contract with make a move note
+    // -------------------------------------------------------------------------
+    println!("\n[STEP 7] Compose make a move note");
+
+    let assembler: Assembler = TransactionKernel::assembler().with_debug_mode(true);
+    let account_component_lib = create_library(
+        assembler.clone(),
+        "external_contract::game_contract",
+        &game_code,
+    )
+    .unwrap();
+
+    let make_a_move_note_code =
+        fs::read_to_string(Path::new("../masm/notes/make_a_move_note.masm")).unwrap();
+    let make_a_move_note_script = ScriptBuilder::new(true)
+        .with_dynamically_linked_library(&account_component_lib)
+        .unwrap()
+        .compile_note_script(make_a_move_note_code)
+        .unwrap();
+
+    let make_a_move_note_inputs = NoteInputs::new(vec![
+        Felt::new(0),
+        Felt::new(0),
+        Felt::new(0),
+        Felt::new(1),
+        Felt::new(7),
+    ])
+    .unwrap();
+    let make_a_move_note_serial_num = client.rng().draw_word();
+    let make_a_move_note_recipient = NoteRecipient::new(
+        make_a_move_note_serial_num,
+        make_a_move_note_script,
+        make_a_move_note_inputs,
+    );
+    let make_a_move_note_tag =
+        NoteTag::for_public_use_case(0, 0, NoteExecutionMode::Local).unwrap();
+    let make_a_move_note_metadata = NoteMetadata::new(
+        alice_account.id(),
+        NoteType::Public,
+        make_a_move_note_tag,
+        NoteExecutionHint::always(),
+        Felt::new(0),
+    )?;
+    let make_a_move_note = Note::new(
+        empty_assets.clone(),
+        make_a_move_note_metadata,
+        make_a_move_note_recipient,
+    );
+
+    println!("Make a move note ID: {:?}", make_a_move_note.id().to_hex());
+
+    // -------------------------------------------------------------------------
+    // STEP 8: Submit make a move note on-chain
+    // -------------------------------------------------------------------------
+    println!("\n[STEP 8] Submit make a move note on-chain");
+
+    // build and submit transaction
+    let note_request = TransactionRequestBuilder::new()
+        .own_output_notes(vec![OutputNote::Full(make_a_move_note.clone())])
+        .build()
+        .unwrap();
+    let tx_result = client
+        .new_transaction(alice_account.id(), note_request)
+        .await
+        .unwrap();
+    let _ = client.submit_transaction(tx_result.clone()).await;
+    client.sync_state().await?;
+
+    println!("Submitted make a move note");
+
+    // -------------------------------------------------------------------------
+    // STEP 9: Consume the make a move note
+    // -------------------------------------------------------------------------
+    println!("\n[STEP 9] Consume the make a move note");
+
+    let consume_make_a_move_note_request = TransactionRequestBuilder::new()
+        .unauthenticated_input_notes([(make_a_move_note, None)])
+        .build()
+        .unwrap();
+    let tx_result = client
+        .new_transaction(game_contract.id(), consume_make_a_move_note_request)
+        .await
+        .unwrap();
+    let _ = client.submit_transaction(tx_result.clone()).await;
+    client.sync_state().await.unwrap();
+
+    println!("Consumed make a move note");
+
     println!(
-        "game contract storage: {:?}",
-        account.unwrap().account().storage().get_item(0)
+        "player1 values mapping storage slot: {:?}",
+        account_data.storage().get_item(2)
+    );
+
+    println!(
+        "player1 values mapping storage slot: {:?}",
+        account_data.storage().get_map_item(
+            2,
+            Word::new([Felt::new(0), Felt::new(0), Felt::new(0), Felt::new(1)].into())
+        )
     );
 
     Ok(())
